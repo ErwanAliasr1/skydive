a3339242277afc09805f82064f988871dd82f939
Erwan Velu <erwan@redhat.com>
2018-10-23 16:45:01 +0200

    topology/ceph: Moving graphOSD(s) function into ceph/osd.go
    It's much more easier to read to have all OSDs related code into osd.go
    
    Signed-off-by: Erwan Velu <erwan@redhat.com>

--- topology/probes/ceph/osd.go
+++ topology/probes/ceph/osd.go
@@ -27,7 +27,12 @@ import (
 	"encoding/base64"
 	"encoding/gob"
 	"encoding/json"
+	"fmt"
 	"os/exec"
+
+	"github.com/skydive-project/skydive/logging"
+	"github.com/skydive-project/skydive/topology"
+	"github.com/skydive-project/skydive/topology/graph"
 )
 
 // OSD structure extracted from 12.2.5-42.el7cp
@@ -93,4 +98,117 @@ func ReadOSD(s *InfoProbe) {
 			s.g.AddMetadata(s.hostNode, "Software.Ceph.OSD.metadata", base64.StdEncoding.EncodeToString(b.Bytes()))
 		}
 	}
+}
+
+// Create a Node per OSD
+func graphOSD(p *Probe, osd OSD) bool {
+	var frontIface *graph.Node
+	var backIface *graph.Node
+	var frontIfaceMetadata graph.Metadata
+	var backIfaceMetadata graph.Metadata
+
+	lookupNode := p.graph.LookupFirstNode(graph.Metadata{
+		"Name": osd.Hostname,
+		"Type": "host",
+	})
+
+	if lookupNode == nil {
+		logging.GetLogger().Errorf("Cannot find any node for host %s", osd.Hostname)
+		return false
+	}
+
+	osdName := fmt.Sprintf("osd.%d", osd.ID)
+	nodeName := fmt.Sprintf("%s_%d", osd.Hostname, osd.ID)
+	metadata := graph.Metadata{
+		"Manager": "ceph",
+		"Type":    "OSD",
+		"Name":    osdName,
+		"Ceph": map[string]interface{}{
+			"OSD": osd,
+		},
+	}
+
+	if len(osd.FrontIface) > 0 {
+		frontIfaceMetadata = graph.Metadata{
+			"Type":         "socket",
+			"Address":      osd.FrontAddr,
+			"RelationType": "frontIface",
+		}
+		frontIface = p.graph.LookupFirstChild(lookupNode, graph.Metadata{"Name": osd.FrontIface})
+		if frontIface == nil {
+			logging.GetLogger().Errorf("%s:  Missing FrontIface %s for %s", osd.Hostname, osd.FrontIface, osdName)
+			return false
+		}
+	}
+
+	if len(osd.BackIface) > 0 {
+		backIfaceMetadata = graph.Metadata{
+			"Type":         "socket",
+			"Address":      osd.BackAddr,
+			"RelationType": "backIface",
+		}
+		backIface = p.graph.LookupFirstChild(lookupNode, graph.Metadata{"Name": osd.BackIface})
+		if backIface == nil {
+			logging.GetLogger().Errorf("%s:  Missing BackIface %s for %s", osd.Hostname, osd.BackIface, osdName)
+			return false
+		}
+	}
+
+	// Conecting the OSD to the host
+	logging.GetLogger().Infof("%s: Adding OSD %s", osd.Hostname, osdName)
+	containerNode := p.graph.NewNode(graph.Identifier(nodeName), metadata)
+	topology.AddOwnershipLink(p.graph, lookupNode, containerNode, nil)
+
+	// Connect any back or front interface to the OSD
+	if backIface != nil {
+		p.graph.Link(containerNode, backIface, backIfaceMetadata)
+	}
+	if frontIface != nil {
+		p.graph.Link(containerNode, frontIface, frontIfaceMetadata)
+	}
+	return true
+}
+
+func graphOSDs(p *Probe, n *graph.Node) bool {
+	var osds []OSD
+	if metadata, _ := n.GetField("Software.Ceph.OSD.metadata"); metadata != nil {
+		if p.clusters[p.cluster.Fsid] == metadata.(string) {
+			logging.GetLogger().Infof("Cluster ceph %s is already graphed", p.cluster.Fsid)
+			return false
+		}
+		by, err := base64.StdEncoding.DecodeString(metadata.(string))
+		if err != nil {
+			logging.GetLogger().Errorf(`failed base64 Decode : %s`, err)
+			return false
+		}
+		b := bytes.Buffer{}
+		b.Write(by)
+		d := gob.NewDecoder(&b)
+		if err := d.Decode(&osds); err != nil {
+			logging.GetLogger().Errorf(`failed to Decode : %s`, err)
+			return false
+		}
+		if len(osds) > 0 {
+			//logging.GetLogger().Infof("onNodeEvent Received %#v", osds)
+			everythingGraphed := true
+			for _, osd := range osds {
+				if len(osd.Hostname) == 0 {
+					continue
+				}
+				graphed := graphOSD(p, osd)
+				if (graphed == false) && (everythingGraphed == true) {
+					everythingGraphed = false
+				}
+			}
+			if everythingGraphed == false {
+				logging.GetLogger().Infof("OSD graphing of cluster %s aborted because of missing nodes", p.cluster.Fsid)
+				return false
+			}
+			// This is the only place where we know the cluster is perfectly rendered
+			p.clusters[p.cluster.Fsid] = metadata.(string)
+			logging.GetLogger().Infof("Ceph cluster %s is rendered", p.cluster.Fsid)
+			return true
+		}
+	}
+	return false
 }
